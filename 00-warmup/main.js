ea098097afe3743ad40e61cd3a2ffd9fe574356c
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-09-10 20:18:37 -0400

    geo api warmup

--- 00-warmup/main.js
+++ 00-warmup/main.js
@@ -1,9 +1,10 @@
 const METEO_COORD_URL = "https://geocoding-api.open-meteo.com/v1/search?name=_METEO_CITY_&count=10&language=en&format=json";
 const METEO_WEATHER_URL = "https://archive-api.open-meteo.com/v1/archive?latitude=_METEO_LAT_&longitude=_METEO_LON_&start_date=2026-08-01&end_date=2026-08-28&hourly=temperature_2m";
 
-const NYT_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=_NYT_QUERY_&api-key=${NYT_KEY}`;
-const SI_URL = `https://api.si.edu/openaccess/api/v1.0/search?q=_SI_QUERY_ AND online_media&api_key=${SI_KEY}`;
-const GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/interactions";
+// Keep these commented out for now
+// const NYT_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=_NYT_QUERY_&api-key=${NYT_KEY}`;
+// const SI_URL = `https://api.si.edu/openaccess/api/v1.0/search?q=_SI_QUERY_ AND online_media&api_key=${SI_KEY}`;
+// const GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/interactions";
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -15,8 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
     // replace _METEO_CITY_ with city name in the url
     const urlWithCity = METEO_COORD_URL.replace("_METEO_CITY_", cityName);
 
-    // TODO: fetch, json
-    //       populate html elements with lat/lon values
+    // fetch, json
+    const latlonRes = await fetch(urlWithCity);
+    const latlonData = await latlonRes.json();
+
+    const firstCityData = latlonData.results[0];
+
+    // populate html elements with lat/lon values
+    document.querySelector("#lat-result").innerHTML = firstCityData.latitude;
+    document.querySelector("#lon-result").innerHTML = firstCityData.longitude;
   });
 
   // TODO: NYT
